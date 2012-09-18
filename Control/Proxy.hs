c84594bfb9921b3853e587dfc8fea6e4e3a7770b
Gabriel Gonzalez <Gabriel439@gmail.com>
2012-09-18 14:26:42 -0700

    Added Control.Proxy.Trans to Control.Proxy exports

--- Control/Proxy.hs
+++ Control/Proxy.hs
@@ -5,12 +5,14 @@ module Control.Proxy (
     -- $modules
     module Control.Proxy.Class,
     module Control.Proxy.Core,
-    module Control.Proxy.Pipe
+    module Control.Proxy.Pipe,
+    module Control.Proxy.Trans
     ) where
 
 import Control.Proxy.Class
 import Control.Proxy.Core
 import Control.Proxy.Pipe
+import Control.Proxy.Trans
 
 {- $modules
     "Control.Proxy.Core" provides the core type and primitives.
