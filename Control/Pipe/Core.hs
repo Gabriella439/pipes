5026e49890c29d887166141356808f746f63ed97
Gabriel Gonzalez <Gabriel439@gmail.com>
2012-10-05 14:41:23 -0700

    Updated documentation for Control.Pipe.Core

--- Control/Pipe/Core.hs
+++ Control/Pipe/Core.hs
@@ -4,9 +4,6 @@
 -}
 
 module Control.Pipe.Core (
-    -- * Introduction
-    -- $summary
-
     -- * Types
     -- $types
     PipeF(..),
@@ -31,28 +28,14 @@ module Control.Pipe.Core (
     runPipe
     ) where
 
-import Control.Applicative
-import Control.Category
+import Control.Applicative (Applicative(pure, (<*>)))
+import Control.Category (Category((.), id), (<<<), (>>>))
 import Control.Monad (forever)
 import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Free
+import Control.Monad.Trans.Free (
+    FreeF(Free, Pure), FreeT(FreeT, runFreeT), wrap)
 import Data.Closed (C)
 import Prelude hiding ((.), id)
-
-{- $summary
-    I completely expose the 'Pipe' data type and internals in order to encourage
-    people to write their own 'Pipe' functions.  This does not compromise the
-    correctness or safety of the library at all and you can feel free to use the
-    constructors directly without violating any laws or invariants.
-
-    I promote using the 'Monad' and 'Category' instances to build and compose
-    pipes, but this does not mean that they are the only option.  In fact, any
-    combinator provided by other iteratee libraries can be recreated for pipes,
-    too.  However, this core library does not provide many of the functions
-    found in other libraries in order to encourage people to find principled and
-    theoretically grounded solutions rather than devise ad-hoc solutions
-    characteristic of other iteratee implementations.
--}
 
 {- $types
     The 'Pipe' type is strongly inspired by Mario Blazevic's @Coroutine@ type in
@@ -135,57 +118,21 @@ pipe :: (Monad m) => (a -> b) -> Pipe a b m r
 pipe f = forever $ await >>= yield . f
 
 {- $category
-    'Pipe's form a 'Category', meaning that you can compose 'Pipe's and also
-    define an identity 'Pipe'.
+    'Pipe's form a 'Category', meaning that you can compose 'Pipe's using
+    ('<+<') and also define an identity 'Pipe': 'idP'.  These satisfy the
+    category laws:
+
+> idP <+< p = p
+>
+> p <+< idP = p
+>
+> (p1 <+< p2) <+< p3 = p1 <+< (p2 <+< p3)
 
     'Pipe' composition binds the output of the upstream 'Pipe' to the input of
     the downstream 'Pipe'.  Like Haskell functions, 'Pipe's are lazy, meaning
     that upstream 'Pipe's are only evaluated as far as necessary to generate
     enough input for downstream 'Pipe's.  If any 'Pipe' terminates, it also
     terminates every 'Pipe' composed with it.
-
-    If you want to define a proper 'Category' instance you have to wrap the
-    'Pipe' type using the newtype 'PipeC' in order to rearrange the type
-    variables.
-
-    This means that if you want to compose pipes using ('.') from the 'Category'
-    type class, you end up with a newtype mess:
-
-> unPipeC (PipeC p1 . PipeC p2)
-
-    You can avoid this by using convenient operators that do this newtype
-    wrapping and unwrapping for you:
-
-> p1 <+< p2 = unPipeC $ PipeC p1 . PipeC p2
->
-> idP = unPipeC id
-
-    The 'Category' instance obeys the 'Category' laws.  In other words:
-
-    * Composition is truly associative.  The result of composition produces the
-      exact same composite 'Pipe' regardless of how you group composition, so it
-      is perfectly safe to omit the parentheses altogether:
-
-> (p1 <+< p2) <+< p3 = p1 <+< (p2 <+< p3) = p1 <+< p2 <+< p3
-
-    * 'idP' is a true identity pipe.  Composing a pipe with 'idP' returns the
-      exact same original pipe:
-
-> p <+< idP = p
-> idP <+< p = p
-
-    The 'Category' laws are \"correct by construction\", meaning that you cannot
-    break them despite the library's internals being fully exposed.  The above
-    equalities are true using the strongest denotational semantics possible in
-    Haskell, namely that both sides of the equals sign correspond to the exact
-    same value in Haskell, constructor-for-constructor, value-for-value.  You
-    cannot create a function that can distinguish the results.
-
-    Actually, all other class instances in this library provide the same strong
-    guarantees for their corresponding laws.  I only emphasize the guarantee for
-    the 'Category' instance because it is one of the most distinguishing
-    features of this library, making it far more extensible than other
-    implementations.
 -}
 
 -- | 'Pipe's form a 'Category' instance when you rearrange the type variables
@@ -227,16 +174,8 @@ idP = pipe id
     Note that you can also unwrap a 'Pipe' a single step at a time using
     'runFreeT' (since 'Pipe' is just a type synonym for a free monad
     transformer).  This will take you to the next /external/ 'await' or 'yield'
-    statement.
-
-    This means that a closed 'Pipeline' will unwrap to a single step, in which
-    case you would have been better served by 'runPipe'.  This directly follows
-    from the 'Category' laws, which guarantee that you cannot resolve a
-    composite pipe into its component pipes.  When you compose two pipes, the
-    internal await and yield statements fuse and completely disappear.
-
-    'runFreeT' is ideal for more advanced users who wish to write their own
-    'Pipe' functions while waiting for me to find more elegant solutions.
+    statement.  This means that a closed 'Pipeline' will unwrap to a single
+    step, in which case you would have been better served by 'runPipe'.
 -}
 {-|
     Run the 'Pipe' monad transformer, converting it back into the base monad.
@@ -252,11 +191,10 @@ idP = pipe id
 
     * It prevents against accidental data loss.
 
-    * It prevents wastefully draining a scarce resource by gratuitously
-      demanding values from it.
+    * It protects against silent failures
 
-    * It encourages an idiomatic pipe programming style where input is consumed
-      in a structured way using a 'Consumer'.
+    * It prevents wastefully draining a scarce resource by gratuitously
+      driving it to completion
 
     If you believe that discarding output is the appropriate behavior, you can
     specify this by explicitly feeding your output to a pipe that gratuitously
