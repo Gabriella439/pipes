41ffac59923a934770567bb6616515b0c1381219
Gabriel Gonzalez <Gabriel439@gmail.com>
2012-10-06 13:43:02 -0700

    Added module descriptions to Control.Proxy.Prelude

--- Control/Proxy/Prelude.hs
+++ Control/Proxy/Prelude.hs
@@ -1,6 +1,7 @@
 -- | Entry point for the Control.Proxy.Prelude hierarchy
 
 module Control.Proxy.Prelude (
+    -- $modules
     module Control.Proxy.Prelude.Base,
     module Control.Proxy.Prelude.IO,
     module Control.Proxy.Prelude.Kleisli
@@ -9,3 +10,12 @@ module Control.Proxy.Prelude (
 import Control.Proxy.Prelude.Base
 import Control.Proxy.Prelude.IO
 import Control.Proxy.Prelude.Kleisli
+
+{- $modules
+    "Control.Proxy.Prelude.Base" provides pure utility proxies.
+
+    "Control.Proxy.Prelude.IO" provides proxies for simple 'IO'.
+
+    "Control.Proxy.Prelude.Kleisli" provides convenience functions for working
+    with Kleisli arrows.
+-}
