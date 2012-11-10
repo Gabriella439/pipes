9d134d8866ebef64712dcff12404590d30a047cd
Gabriel Gonzalez <Gabriel439@gmail.com>
2012-11-10 14:12:09 -0800

    Inlined Control.Proxy.Trans.Reader and polished source code

--- Control/Proxy/Trans/Reader.hs
+++ Control/Proxy/Trans/Reader.hs
@@ -28,53 +28,71 @@ import Control.Proxy.Trans (ProxyTrans(liftP))
 newtype ReaderP i p a' a b' b (m :: * -> *) r
   = ReaderP { unReaderP :: i -> p a' a b' b m r }
 
-instance (Monad (p a' a b' b m)) => Functor (ReaderP i p a' a b' b m) where
-    fmap = liftM
+instance (Functor           (p a' a b' b m))
+       => Functor (ReaderP i p a' a b' b m) where
+    fmap f p = ReaderP (\i -> fmap f (unReaderP p i))
 
-instance (Monad (p a' a b' b m)) => Applicative (ReaderP i p a' a b' b m) where
-    pure  = return
-    (<*>) = ap
+instance (Applicative           (p a' a b' b m))
+       => Applicative (ReaderP i p a' a b' b m) where
+    pure r = ReaderP (\_ -> pure r)
+    p1 <*> p2 = ReaderP (\i -> unReaderP p1 i <*> unReaderP p2 i)
 
-instance (Monad (p a' a b' b m)) => Monad (ReaderP i p a' a b' b m) where
-    return a = ReaderP $ \_ -> return a
-    m >>= f = ReaderP $ \i -> do
+instance (Monad           (p a' a b' b m))
+       => Monad (ReaderP i p a' a b' b m) where
+    return r = ReaderP (\_ -> return r)
+    m >>= f  = ReaderP (\i -> do
         a <- unReaderP m i
-        unReaderP (f a) i
+        unReaderP (f a) i )
 
-instance (MonadPlus (p a' a b' b m))
- => Alternative (ReaderP i p a' a b' b m) where
-    empty = mzero
-    (<|>) = mplus
+instance (Alternative           (p a' a b' b m))
+       => Alternative (ReaderP i p a' a b' b m) where
+    empty = ReaderP (\_ -> empty)
+    p1 <|> p2 = ReaderP (\i -> unReaderP p1 i <|> unReaderP p2 i)
 
-instance (MonadPlus (p a' a b' b m))
- => MonadPlus (ReaderP i p a' a b' b m) where
-    mzero = ReaderP $ \_ -> mzero
-    mplus m1 m2 = ReaderP $ \i -> mplus (unReaderP m1 i) (unReaderP m2 i)
+instance (MonadPlus           (p a' a b' b m))
+       => MonadPlus (ReaderP i p a' a b' b m) where
+    mzero = ReaderP (\_ -> mzero)
+    mplus m1 m2 = ReaderP (\i -> mplus (unReaderP m1 i) (unReaderP m2 i))
 
-instance (MonadTrans (p a' a b' b)) => MonadTrans (ReaderP i p a' a b' b) where
-    lift m = ReaderP $ \_ -> lift m
+instance (MonadTrans           (p a' a b' b))
+       => MonadTrans (ReaderP i p a' a b' b) where
+    lift m = ReaderP (\_ -> lift m)
 
-instance (MonadIO (p a' a b' b m)) => MonadIO (ReaderP i p a' a b' b m) where
-    liftIO m = ReaderP $ \_ -> liftIO m
+instance (MonadIO           (p a' a b' b m))
+       => MonadIO (ReaderP i p a' a b' b m) where
+    liftIO m = ReaderP (\_ -> liftIO m)
 
-instance (MFunctor (p a' a b' b)) => MFunctor (ReaderP i p a' a b' b) where
-    mapT nat = ReaderP . fmap (mapT nat) . unReaderP
+instance (MFunctor           (p a' a b' b))
+       => MFunctor (ReaderP i p a' a b' b) where
+    mapT nat p = ReaderP (\i -> mapT nat (unReaderP p i))
+ -- mapT nat = ReaderP . fmap (mapT nat) . unReaderP
 
-instance (Channel p) => Channel (ReaderP i p) where
-    idT a = ReaderP $ \_ -> idT a
-    (p1 >-> p2) a = ReaderP $ \i ->
-        ((`unReaderP` i) . p1 >-> (`unReaderP` i) . p2) a
+instance (Channel            p  )
+       => Channel (ReaderP i p) where
+    idT a = ReaderP (\_ -> idT a)
+    p1 >-> p2 = \c'1 -> ReaderP (\i ->
+        ((\b'  -> unReaderP (p1 b' ) i)
+     >-> (\c'2 -> unReaderP (p2 c'2) i) ) c'1 )
+ {- p1 >-> p2 = \c' -> ReaderP $ \i ->
+        ((`unReaderP` i) . p1 >-> (`unReaderP` i) . p2) c' -}
 
-instance (Interact p) => Interact (ReaderP i p) where
-    request a = ReaderP $ \_ -> request a
-    (p1 \>\ p2) a = ReaderP $ \i ->
-        ((`unReaderP` i) . p1 \>\ (`unReaderP` i) . p2) a
-    respond a = ReaderP $ \_ -> respond a
-    (p1 />/ p2) a = ReaderP $ \i ->
-        ((`unReaderP` i) . p1 />/ (`unReaderP` i) . p2) a
+instance (Interact            p )
+       => Interact (ReaderP i p) where
+    request a = ReaderP (\_ -> request a)
+ {- p1 \>\ p2 = \a -> ReaderP $ \i ->
+        ((`unReaderP` i) . p1 \>\ (`unReaderP` i) . p2) a -}
+    p1 \>\ p2 = \c'1 -> ReaderP (\i ->
+        ((\b'  -> unReaderP (p1 b' ) i)
+     \>\ (\c'2 -> unReaderP (p2 c'2) i) ) c'1 )
+    respond a = ReaderP (\_ -> respond a)
+ {- p1 />/ p2 = \a -> ReaderP $ \i ->
+        ((`unReaderP` i) . p1 />/ (`unReaderP` i) . p2) a -}
+    p1 />/ p2 = \a1 -> ReaderP (\i ->
+        ((\b  -> unReaderP (p1 b ) i)
+     />/ (\a2 -> unReaderP (p2 a2) i) ) a1 )
 
 instance ProxyTrans (ReaderP i) where
-    liftP m = ReaderP $ \_ -> m
+    liftP m = ReaderP (\_ -> m)
 
 -- | Run a 'ReaderP' computation, supplying the environment
 runReaderP :: i -> ReaderP i p a' a b' b m r -> p a' a b' b m r
@@ -82,13 +100,15 @@ runReaderP i m = unReaderP m i
 
 -- | Run a 'ReaderP' \'@K@\'leisli arrow, supplying the environment
 runReaderK :: i -> (q -> ReaderP i p a' a b' b m r) -> (q -> p a' a b' b m r)
-runReaderK i = (runReaderP i .)
+runReaderK i p q = runReaderP i (p q)
+-- runReaderK i = (runReaderP i .)
 
 -- | Modify a computation's environment (a more general version of 'local')
 withReaderP
  :: (Monad (p a' a b' b m))
  => (j -> i) -> ReaderP i p a' a b' b m r -> ReaderP j p a' a b' b m r
-withReaderP f r = ReaderP $ unReaderP r . f
+withReaderP f p = ReaderP (\i -> unReaderP p (f i))
+-- withReaderP f p = ReaderP $ unReaderP p . f
 
 -- | Get the environment
 ask :: (Monad (p a' a b' b m)) => ReaderP i p a' a b' b m i
@@ -96,7 +116,7 @@ ask = ReaderP return
 
 -- | Get a function of the environment
 asks :: (Monad (p a' a b' b m)) => (i -> r) -> ReaderP i p a' a b' b m r
-asks f = ReaderP (return . f)
+asks f = ReaderP (\i -> return (f i))
 
 -- | Modify a computation's environment (a specialization of 'withReaderP')
 local
