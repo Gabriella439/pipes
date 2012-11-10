fcb0f0cf27ab0c6df3d2e3309d64814513df1661
Gabriel Gonzalez <Gabriel439@gmail.com>
2012-11-10 11:15:30 -0800

    Polished Control.Proxy.Trans.Identity

--- Control/Proxy/Trans/Identity.hs
+++ Control/Proxy/Trans/Identity.hs
@@ -21,48 +21,52 @@ import Control.Proxy.Trans (ProxyTrans(liftP))
 newtype IdentityP p a' a b' b (m :: * -> *) r =
     IdentityP { runIdentityP :: p a' a b' b m r }
 
-instance (Functor (p a' a b' b m))
- => Functor (IdentityP p a' a b' b m) where
+instance (Functor           (p a' a b' b m))
+       => Functor (IdentityP p a' a b' b m) where
     fmap f p = IdentityP (fmap f (runIdentityP p))
  -- fmap f = IdentityP . fmap f . runIdentityP
 
-instance (Applicative (p a' a b' b m))
- => Applicative (IdentityP p a' a b' b m) where
+instance (Applicative           (p a' a b' b m))
+       => Applicative (IdentityP p a' a b' b m) where
     pure r = IdentityP (pure r)
  -- pure = IdentityP . pure
 
     fp <*> xp = IdentityP (runIdentityP fp <*> runIdentityP xp)
 
-instance (Monad (p a' a b' b m))
- => Monad (IdentityP p a' a b' b m) where
+instance (Monad           (p a' a b' b m))
+       => Monad (IdentityP p a' a b' b m) where
     return r = IdentityP (return r)
  -- return = IdentityP . return
 
     m >>= f = IdentityP (runIdentityP m >>= \x -> runIdentityP (f x))
 
-instance (Alternative (p a' a b' b m))
- => Alternative (IdentityP p a' a b' b m) where
+instance (Alternative           (p a' a b' b m))
+       => Alternative (IdentityP p a' a b' b m) where
     empty = IdentityP empty
     m1 <|> m2 = IdentityP (runIdentityP m1 <|> runIdentityP m2)
 
-instance (MonadPlus (p a' a b' b m))
- => MonadPlus (IdentityP p a' a b' b m) where
+instance (MonadPlus           (p a' a b' b m))
+       => MonadPlus (IdentityP p a' a b' b m) where
     mzero = IdentityP mzero
     mplus m1 m2 = IdentityP $ mplus (runIdentityP m1) (runIdentityP m2)
 
-instance (MonadTrans (p a' a b' b)) => MonadTrans (IdentityP p a' a b' b) where
+instance (MonadTrans           (p a' a b' b))
+       => MonadTrans (IdentityP p a' a b' b) where
     lift m = IdentityP (lift m)
  -- lift = IdentityP . lift
 
-instance (MonadIO (p a' a b' b m)) => MonadIO (IdentityP p a' a b' b m) where
+instance (MonadIO           (p a' a b' b m))
+       => MonadIO (IdentityP p a' a b' b m) where
     liftIO m = IdentityP (liftIO m)
  -- liftIO = IdentityP . liftIO
 
-instance (MFunctor (p a' a b' b)) => MFunctor (IdentityP p a' a b' b) where
+instance (MFunctor           (p a' a b' b))
+       => MFunctor (IdentityP p a' a b' b) where
     mapT nat p = IdentityP (mapT nat (runIdentityP p))
  -- mapT nat = IdentityP . mapT nat . runIdentityP
 
-instance (Channel p) => Channel (IdentityP p) where
+instance (Channel            p )
+       => Channel (IdentityP p) where
     idT = \a' -> IdentityP (idT a')
  -- idT = IdentityP . idT
 
@@ -71,7 +75,8 @@ instance (Channel p) => Channel (IdentityP p) where
      >-> (\b'  -> runIdentityP (p2 b' )) ) c'1 )
  -- p1 >-> p2 = (IdentityP .) $ runIdentityP . p1 >-> runIdentityP . p2
 
-instance (Interact p) => Interact (IdentityP p) where
+instance (Interact            p )
+       => Interact (IdentityP p) where
     request a' = IdentityP (request a')
  -- request = IdentityP . request
 
