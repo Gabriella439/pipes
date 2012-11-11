320a85f5c481acd8039d031eebee6216867e680b
Gabriel Gonzalez <Gabriel439@gmail.com>
2012-11-10 17:38:43 -0800

    Restored Control.Proxy.Trans.Identity to Monad constraints

--- Control/Proxy/Trans/Identity.hs
+++ Control/Proxy/Trans/Identity.hs
@@ -21,34 +21,41 @@ import Control.Proxy.Trans (ProxyTrans(liftP))
 newtype IdentityP p a' a b' b (m :: * -> *) r =
     IdentityP { runIdentityP :: p a' a b' b m r }
 
-instance (Functor           (p a' a b' b m))
+instance (Monad             (p a' a b' b m))
        => Functor (IdentityP p a' a b' b m) where
-    fmap f p = IdentityP (fmap f (runIdentityP p))
- -- fmap f = IdentityP . fmap f . runIdentityP
+    fmap f p = IdentityP (do
+        x <- runIdentityP p
+        return (f x) )
+ -- fmap = liftM
 
-instance (Applicative           (p a' a b' b m))
+instance (Monad                 (p a' a b' b m))
        => Applicative (IdentityP p a' a b' b m) where
-    pure r = IdentityP (pure r)
- -- pure = IdentityP . pure
+    pure = return
 
-    fp <*> xp = IdentityP (runIdentityP fp <*> runIdentityP xp)
+    fp <*> xp = IdentityP (do
+        f <- runIdentityP fp
+        x <- runIdentityP xp
+        return (f x) )
+ -- fp <*> xp = ap
 
 instance (Monad           (p a' a b' b m))
        => Monad (IdentityP p a' a b' b m) where
     return r = IdentityP (return r)
  -- return = IdentityP . return
 
-    m >>= f = IdentityP (runIdentityP m >>= \x -> runIdentityP (f x))
+    m >>= f = IdentityP (do
+        x <- runIdentityP m
+        runIdentityP (f x) )
 
-instance (Alternative           (p a' a b' b m))
+instance (MonadPlus             (p a' a b' b m))
        => Alternative (IdentityP p a' a b' b m) where
-    empty = IdentityP empty
-    m1 <|> m2 = IdentityP (runIdentityP m1 <|> runIdentityP m2)
+    empty = mzero
+    (<|>) = mplus
 
 instance (MonadPlus           (p a' a b' b m))
        => MonadPlus (IdentityP p a' a b' b m) where
     mzero = IdentityP mzero
-    mplus m1 m2 = IdentityP $ mplus (runIdentityP m1) (runIdentityP m2)
+    mplus m1 m2 = IdentityP (mplus (runIdentityP m1) (runIdentityP m2))
 
 instance (MonadTrans           (p a' a b' b))
        => MonadTrans (IdentityP p a' a b' b) where
@@ -77,7 +84,7 @@ instance (Channel            p )
 
 instance (Interact            p )
        => Interact (IdentityP p) where
-    request a' = IdentityP (request a')
+    request = \a' -> IdentityP (request a')
  -- request = IdentityP . request
 
     p1 \>\ p2 = \c'1 -> IdentityP (
@@ -85,11 +92,12 @@ instance (Interact            p )
      \>\ (\c'2 -> runIdentityP (p2 c'2)) ) c'1 )
  -- p1 \>\ p2 = (IdentityP .) $ runIdentityP . p1 \>\ runIdentityP . p2
 
-    respond b = IdentityP (respond b )
+    respond = \b -> IdentityP (respond b)
  -- respond = IdentityP . respond
 
     p1 />/ p2 = \a1 -> IdentityP (
-        ((\a2 -> runIdentityP (p1 a2)) />/ (\b -> runIdentityP (p2 b))) a1 )
+        ((\a2 -> runIdentityP (p1 a2))
+     />/ (\b  -> runIdentityP (p2 b )) ) a1 )
  -- p1 />/ p2 = (IdentityP .) $ runIdentityP . p1 />/ runIdentityP . p2
 
 instance ProxyTrans IdentityP where
@@ -97,5 +105,5 @@ instance ProxyTrans IdentityP where
 
 -- | Run an 'IdentityP' \'@K@\'leisli arrow
 runIdentityK :: (q -> IdentityP p a' a b' b m r) -> (q -> p a' a b' b m r)
-runIdentityK k = \q -> runIdentityP (k q)
+runIdentityK k q = runIdentityP (k q)
 -- runIdentityK = (runIdentityP .)
