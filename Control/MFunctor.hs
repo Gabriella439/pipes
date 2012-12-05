ac4a8fd76bf9b811aa86e77ec2819b1d36e5c63f
Gabriel Gonzalez <Gabriel439@gmail.com>
2012-12-05 13:32:13 -0500

    Added transformers instances for the MFunctor class

--- Control/MFunctor.hs
+++ Control/MFunctor.hs
@@ -13,8 +13,41 @@ module Control.MFunctor (
     MFunctor(..)
     ) where
 
+import Control.Monad.Trans.Identity
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.RWS
+import qualified Control.Monad.Trans.State.Strict as StateStrict
+import qualified Control.Monad.Trans.State.Lazy   as StateLazy 
+import qualified Control.Monad.Trans.Writer.Strict as WriterStrict
+import qualified Control.Monad.Trans.Writer.Lazy   as WriterLazy
+
 -- | A functor in the category of monads
 class MFunctor t where
     {-| Lift a monad morphism from @m@ to @n@ into a monad morphism from
         @(t m)@ to @(t n)@ -}
     hoist :: (Monad m) => (forall a . m a -> n a) -> t m b -> t n b
+
+instance MFunctor IdentityT where
+    hoist nat = mapIdentityT nat
+
+instance MFunctor MaybeT where
+    hoist nat = mapMaybeT nat
+
+instance MFunctor (ReaderT r) where
+    hoist nat = mapReaderT nat
+
+instance MFunctor (RWST r w s) where
+    hoist nat = mapRWST nat
+
+instance MFunctor (StateStrict.StateT s) where
+    hoist nat = StateStrict.mapStateT nat
+
+instance MFunctor (StateLazy.StateT s) where
+    hoist nat = StateLazy.mapStateT nat
+
+instance MFunctor (WriterStrict.WriterT w) where
+    hoist nat = WriterStrict.mapWriterT nat
+
+instance MFunctor (WriterLazy.WriterT w) where
+    hoist nat = WriterLazy.mapWriterT nat
