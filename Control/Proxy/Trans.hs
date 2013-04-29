22f4922de211218729d5419f8127a0f525f5d30e
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-04-28 20:45:04 -0700

    Updated documentation

--- Control/Proxy/Trans.hs
+++ Control/Proxy/Trans.hs
@@ -7,11 +7,7 @@
     
 module Control.Proxy.Trans (
     -- * Proxy Transformers
-    ProxyTrans(..),
-    mapP
-
-    -- * Laws
-    -- $laws
+    ProxyTrans(..)
     ) where
 
 import Control.Proxy.Class (Proxy)
@@ -19,54 +15,3 @@ import Control.Proxy.Class (Proxy)
 -- | Uniform interface to lifting proxies
 class ProxyTrans t where
     liftP :: (Monad m, Proxy p) => p a' a b' b m r -> t p a' a b' b m r
-
-{-| Lift a 'Proxy' Kleisli arrow
-
-> mapP = (lift .)
--}
-mapP :: (Monad m, Proxy p, ProxyTrans t)
-     => (q -> p a' a b' b m r) -> (q -> t p a' a b' b m r)
-mapP = (liftP .)
-{-# INLINABLE mapP #-}
-
-{- $laws
-     'mapP' defines a functor that preserves five categories:
-
-    * Kleisli category
-
-    * The two Proxy categories
-
-    * \"request\" category
-
-    * \"respond\" category
-
-    Laws:
-
-    * Functor between 'Proxy' categories
-
-> mapP (f >-> g) = mapP f >-> mapP g
->
-> mapP idT = idT
-
-> mapP (f >~> g) = mapP f >~> mapP g
->
-> mapP idPush = idPush
-
-    * Functor between Kleisli categories
-
-> mapP (f <=< g) = mapP f <=< mapP g
->
-> mapP return = return
-
-    * Functor between \"request\" categories
-
-> mapP (f /</ g) = mapP f /</ mapP g -- when /</ is defined
->
-> mapP request = request
-
-    * Functor between \"respond\" categories
-
-> mapP (f \<\ g) = mapP f \<\ mapP g -- when \<\ is defined
->
-> mapP respond = respond
--}
