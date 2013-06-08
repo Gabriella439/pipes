34845bc0665ddbc232d89ebb58729f520b1793d2
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-06-08 16:30:25 -0700

    Re-exported proxy transformer run functions from Control.Proxy.Core

--- Control/Proxy/Core.hs
+++ Control/Proxy/Core.hs
@@ -7,6 +7,12 @@ module Control.Proxy.Core (
     module Control.Proxy.Prelude,
     module Control.Proxy.Trans,
     module Control.Proxy.Trans.Identity,
+    module Control.Proxy.Trans.Codensity,
+    module Control.Proxy.Trans.Either,
+    module Control.Proxy.Trans.Maybe,
+    module Control.Proxy.Trans.Reader,
+    module Control.Proxy.Trans.State,
+    module Control.Proxy.Trans.Writer,
     module Control.Proxy.Morph,
     module Control.Monad,
     module Control.Monad.Trans.Class,
@@ -21,6 +27,14 @@ import Control.Proxy.Morph
 import Control.Proxy.Prelude
 import Control.Proxy.Trans
 import Control.Proxy.Trans.Identity
+import Control.Proxy.Trans.Codensity (runCodensityP, runCodensityK)
+import Control.Proxy.Trans.Either (runEitherP, runEitherK)
+import Control.Proxy.Trans.Maybe (runMaybeP, runMaybeK)
+import Control.Proxy.Trans.Reader (runReaderP, runReaderK)
+import Control.Proxy.Trans.State (
+    runStateP, runStateK, evalStateP, evalStateK, execStateP, execStateK )
+import Control.Proxy.Trans.Writer (
+    runWriterP, runWriterK, execWriterP, execWriterK )
 
 {- $modules
     "Control.Proxy.Class" defines the 'Proxy' type class that lets you program
@@ -33,6 +47,9 @@ import Control.Proxy.Trans.Identity
 
     "Control.Proxy.Trans.Identity" exports 'runIdentityP', which substantially
     eases writing completely polymorphic proxies.
+
+    The remaining @Control.Proxy.Trans.*@ modules export run functions for every
+    proxy transformer.
 
     "Control.Proxy.Morph" exports 'hoistP'.
 
