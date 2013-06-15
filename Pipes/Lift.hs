c4e21941c0683ff5937d6bf615a186032b7b4a60
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-06-15 07:10:52 -0700

    Added `liftCatch` and qualified Pipes.Lift imports

--- Pipes/Lift.hs
+++ Pipes/Lift.hs
@@ -17,18 +17,18 @@ module Pipes.Lift (
     runMaybeP
     ) where
 
-import Control.Monad.Trans.Error
-import Control.Monad.Trans.Maybe
-import Control.Monad.Trans.State.Strict
-import Control.Monad.Trans.Writer.Strict
-import Data.Monoid
+import qualified Control.Monad.Trans.Error as E
+import qualified Control.Monad.Trans.Maybe as M
+import qualified Control.Monad.Trans.State.Strict as S
+import qualified Control.Monad.Trans.Writer.Strict as W
+import Data.Monoid (Monoid(mempty, mappend))
 import Pipes.Core
 import Prelude hiding (catch)
 
 -- | Run 'StateT' in the base monad
 runStateP
     :: (Monad m)
-    => s -> Proxy a' a b' b (StateT s m) r -> Proxy a' a b' b m (r, s)
+    => s -> Proxy a' a b' b (S.StateT s m) r -> Proxy a' a b' b m (r, s)
 runStateP = go
   where
     go s p = case p of
@@ -36,26 +36,26 @@ runStateP = go
         Respond b  fb' -> Respond b  (\b' -> go s (fb' b'))
         Pure    r      -> Pure (r, s)
         M          m   -> M (do
-            (p', s') <- runStateT m s
+            (p', s') <- S.runStateT m s
             return (go s' p') )
 {-# INLINABLE runStateP #-}
 
--- | Evaluate 'StateT' in the base monad
+-- | Evaluate 'S.StateT' in the base monad
 evalStateP
-    :: (Monad m) => s -> Proxy a' a b' b (StateT s m) r -> Proxy a' a b' b m r
+    :: (Monad m) => s -> Proxy a' a b' b (S.StateT s m) r -> Proxy a' a b' b m r
 evalStateP s = fmap fst . runStateP s
 {-# INLINABLE evalStateP #-}
 
--- | Execute 'StateT' in the base monad
+-- | Execute 'S.StateT' in the base monad
 execStateP
-    :: (Monad m) => s -> Proxy a' a b' b (StateT s m) r -> Proxy a' a b' b m s
+    :: (Monad m) => s -> Proxy a' a b' b (S.StateT s m) r -> Proxy a' a b' b m s
 execStateP s = fmap snd . runStateP s
 {-# INLINABLE execStateP #-}
 
--- | Run 'WriterT' in the base monad
+-- | Run 'W.WriterT' in the base monad
 runWriterP
     :: (Monad m, Monoid w)
-    => Proxy a' a b' b (WriterT w m) r -> Proxy a' a b' b m (r, w)
+    => Proxy a' a b' b (W.WriterT w m) r -> Proxy a' a b' b m (r, w)
 runWriterP = go mempty
   where
     go w p = case p of
@@ -63,22 +63,22 @@ runWriterP = go mempty
         Respond b  fb' -> Respond b  (\b' -> go w (fb' b'))
         Pure    r      -> Pure (r, w)
         M          m   -> M (do
-            (p', w') <- runWriterT m
+            (p', w') <- W.runWriterT m
             let wt = mappend w w'
             wt `seq` return (go wt p') )
 {-# INLINABLE runWriterP #-}
 
--- | Execute 'WriterT' in the base monad
+-- | Execute 'W.WriterT' in the base monad
 execWriterP
     :: (Monad m, Monoid w)
-    => Proxy a' a b' b (WriterT w m) r -> Proxy a' a b' b m w
+    => Proxy a' a b' b (W.WriterT w m) r -> Proxy a' a b' b m w
 execWriterP = fmap snd . runWriterP
 {-# INLINABLE execWriterP #-}
 
--- | Run 'ErrorT' in the base monad
+-- | Run 'E.ErrorT' in the base monad
 runErrorP
     :: (Monad m)
-    => Proxy a' a b' b (ErrorT e m) r -> Proxy a' a b' b m (Either e r)
+    => Proxy a' a b' b (E.ErrorT e m) r -> Proxy a' a b' b m (Either e r)
 runErrorP = go
   where
     go p = case p of
@@ -86,7 +86,7 @@ runErrorP = go
         Respond b  fb' -> Respond b  (\b' -> go (fb' b'))
         Pure    r      -> Pure (Right r)
         M          m   -> M (do
-            x <- runErrorT m
+            x <- E.runErrorT m
             return (case x of
                 Left  e  -> Pure (Left e)
                 Right p' -> go p' ) )
@@ -95,17 +95,17 @@ runErrorP = go
 -- | Catch an error in the base monad
 catch
     :: (Monad m) 
-    => Proxy a' a b' b (ErrorT e m) r
-    -> (e -> Proxy a' a b' b (ErrorT f m) r)
-    -> Proxy a' a b' b (ErrorT f m) r
+    => Proxy a' a b' b (E.ErrorT e m) r
+    -> (e -> Proxy a' a b' b (E.ErrorT f m) r)
+    -> Proxy a' a b' b (E.ErrorT f m) r
 catch p f = go p
   where
     go p = case p of
         Request a' fa  -> Request a' (\a  -> go (fa  a ))
         Respond b  fb' -> Respond b  (\b' -> go (fb' b'))
         Pure   r       -> Pure r
-        M          m   -> M (ErrorT (do
-            x <- runErrorT m
+        M          m   -> M (E.ErrorT (do
+            x <- E.runErrorT m
             return (Right (case x of
                 Left  e  -> f  e
                 Right p' -> go p' )) ))
@@ -126,11 +126,12 @@ liftCatch c p f = go p
         Respond b  fb' -> Respond b  (\b' -> go (fb' b'))
         Pure   r       -> Pure r
         M          m   -> M (c (m >>= return . go) (\e -> return (f e)))
-{-# INLINABLE #-}
+{-# INLINABLE liftCatch #-}
 
--- | Run 'MaybeT' in the base monad
+-- | Run 'M.MaybeT' in the base monad
 runMaybeP
-    :: (Monad m) => Proxy a' a b' b (MaybeT m) r -> Proxy a' a b' b m (Maybe r)
+    :: (Monad m)
+    => Proxy a' a b' b (M.MaybeT m) r -> Proxy a' a b' b m (Maybe r)
 runMaybeP = go
   where
     go p = case p of
@@ -138,7 +139,7 @@ runMaybeP = go
         Respond b  fb' -> Respond b  (\b' -> go (fb' b'))
         Pure    r      -> Pure (Just r)
         M          m   -> M (do
-            x <- runMaybeT m
+            x <- M.runMaybeT m
             return (case x of
                 Nothing -> Pure Nothing
                 Just p' -> go p' ) )
