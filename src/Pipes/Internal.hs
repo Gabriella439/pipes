c0b59c68314b90e66253b96159aa0db9b4395a13
Merijn Verstraaten <merijn@inconsistent.nl>
2013-08-08 14:07:59 +0100

    Add mtl instances for MonadReader, MonadWriter and MonadState.

--- src/Pipes/Internal.hs
+++ src/Pipes/Internal.hs
@@ -18,15 +18,24 @@
     any functions which can violate the monad transformer laws.
 -}
 
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Pipes.Internal (
     Proxy(..),
     observe
     ) where
 
 import Control.Applicative (Applicative(pure, (<*>)))
+import Control.Monad (liftM)
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import Control.Monad.Morph (MFunctor(hoist))
 import Control.Monad.Trans.Class (MonadTrans(lift))
+
+import Control.Monad.Reader (MonadReader(..))
+import Control.Monad.State (MonadState(..))
+import Control.Monad.Writer (MonadWriter(..))
+import Data.Monoid (mempty)
 
 {-| A 'Proxy' is a monad transformer that receives and sends information on both
     an upstream and downstream interface.
@@ -106,6 +115,57 @@ instance MFunctor (Proxy a' a b' b) where
 
 instance (MonadIO m) => MonadIO (Proxy a' a b' b m) where
     liftIO m = M (liftIO (m >>= \r -> return (Pure r)))
+
+instance (MonadReader r m) => MonadReader r (Proxy a' a b' b m) where
+    ask = lift ask
+    local f = go
+        where
+          go p = case p of
+              Request a' fa  -> Request a' (\a  -> go (fa  a ))
+              Respond b  fb' -> Respond b  (\b' -> go (fb' b'))
+              Pure    r      -> Pure r
+              M       m      -> M (go `liftM` local f m)
+    reader = lift . reader
+
+instance (MonadState s m) => MonadState s (Proxy a' a b' b m) where
+    get = lift get
+    put = lift . put
+    state = lift . state
+
+instance (MonadWriter w m) => MonadWriter w (Proxy a' a b' b m) where
+    writer = lift . writer
+    tell = lift . tell
+    listen = go mempty
+        where
+          go w p = case p of
+              Request a' fa  -> Request a' (\a  -> go w (fa  a ))
+              Respond b  fb' -> Respond b  (\b' -> go w (fb' b'))
+              Pure    r      -> Pure (r, w)
+              M       m      -> M ((\(p', w') -> go w' p') `liftM` listen m)
+
+    pass = go
+        where
+          -- Pass needs to happen in the underlying MonadWriter, so we
+          -- can only sensibly do something if the 'w -> w' function is
+          -- produced inside M.
+          --
+          -- If we encounter an M, we first extract the 'w -> w' function
+          -- using extract, then "pass" that in the underlying monad.
+          go p = case p of
+              Request a' fa  -> Request a' (\a  -> go (fa  a ))
+              Respond b  fb' -> Respond b  (\b' -> go (fb' b'))
+              Pure    (r, _) -> Pure r
+              M       m      -> M (pass (extract `liftM` m))
+          -- Extract can only do something sensibly if there is a Pure
+          -- embedded inside the M we're called on. So if we encounter
+          -- a Pure, we lift the modification function out for 'go' to
+          -- 'pass'. If there is no pure, the only sensibly thing is to
+          -- not modify the writer state and recurse.
+          extract p = case p of
+              Request a' fa  -> (Request a' (\a  -> go (fa  a )), id)
+              Respond b  fb' -> (Respond b  (\b' -> go (fb' b')), id)
+              Pure    (r, f) -> (Pure r, f)
+              M       m      -> (go (M m), id)
 
 {-| The monad transformer laws are correct when viewed through the 'observe'
     function:
