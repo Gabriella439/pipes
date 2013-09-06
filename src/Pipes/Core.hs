735b8bca5c767870ea8ffc26e2d31a2be52e1b46
Mihaly Barasz <mihaly@barasz.com>
2013-09-06 15:18:07 +0200

    Better handling of unexpected requests/responses
    Requests/responses leaking out of an Effect (and similarly
    Producers/Consumers) can only be some kind of bottoms. Instead of
    silently ignoring these we evaluate them, so the user is aware of the
    error and gets the original error from the faulty
    Effect/Producer/Consumer.

--- src/Pipes/Core.hs
+++ src/Pipes/Core.hs
@@ -88,7 +88,7 @@ module Pipes.Core (
     (<<+)
     ) where
 
-import Data.Void (Void)
+import Data.Void (Void, absurd)
 import Pipes.Internal (Proxy(..))
 
 {- $proxy
@@ -126,8 +126,8 @@ runEffect :: (Monad m) => Effect m r -> m r
 runEffect = go
   where
     go p = case p of
-        Request _ fa  -> go (fa  ())
-        Respond _ fb' -> go (fb' ())
+        Request v _ -> absurd v
+        Respond v _ -> absurd v
         M       m   -> m >>= go
         Pure    r   -> return r
 {-# INLINABLE runEffect #-}
