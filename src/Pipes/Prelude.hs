f7c2d83ff6cff1c6b3d1c6a66648d12ee47d13e4
Ilya Epifanov <elijah.epifanov@gmail.com>
2014-03-22 14:16:41 +0400

    Added more useful scans with a usage example

--- src/Pipes/Prelude.hs
+++ src/Pipes/Prelude.hs
@@ -51,6 +51,10 @@ module Pipes.Prelude (
     , findIndices
     , scan
     , scanM
+    , scan1
+    , scan1M
+    , scan1i
+    , scan1iM
     , chain
     , read
     , show
@@ -399,6 +403,35 @@ findIndices predicate = loop 0
         when (predicate a) (yield n)
         loop $! n + 1
 {-# INLINABLE findIndices #-}
+
+{-| Strict, endomorphic left scan without explicit initial state.
+
+> -- Compute exponential moving average
+> ema :: (Monad m, Fractional a) => a -> Pipe a a m r
+> ema α = scan1i (\last input -> last * α + input * (1 - α))
+-}
+scan1i :: Monad m => (a -> a -> a) -> Pipe a a m r
+scan1i step = scan1 step id id
+{-# INLINABLE scan1i #-}
+
+{-| Strict, monadic and endomorphic left scan without explicit initial state -}
+scan1iM :: Monad m => (a -> a -> m a) -> Pipe a a m r
+scan1iM step = scan1M step return return
+{-# INLINABLE scan1iM #-}
+
+{-| Strict left scan without explicit initial state -}
+scan1 :: Monad m => (x -> a -> x) -> (a -> x) -> (x -> b) -> Pipe a b m r
+scan1 step begin done = do
+  initial <- await
+  scan step (begin initial) done
+{-# INLINABLE scan1 #-}
+
+{-| Strict, monadic left scan without explicit initial state -}
+scan1M :: Monad m => (x -> a -> m x) -> (a -> m x) -> (x -> m b) -> Pipe a b m r
+scan1M step begin done = do
+  initial <- await
+  scanM step (begin initial) done
+{-# INLINABLE scan1M #-}
 
 {-| Strict left scan
 
