cf9e0aeca39f48a48c0544ccf4166ec606468089
Ben Gamari <ben@smart-cactus.org>
2016-04-18 12:50:42 +0200

    benchmarks: Allow usual Criterion arguments to be used
    Refactor and simplify the command line parsing to allow standard
    Criterion arguments to be used.

--- benchmarks/Common.hs
+++ benchmarks/Common.hs
@@ -1,53 +1,19 @@
 module Common (commonMain) where
 
-import Criterion.Main (Benchmark, defaultMain)
+import Criterion.Main (Benchmark, runMode)
+import Criterion.Main.Options as Criterion
 import Data.Maybe (fromMaybe)
-import System.Console.GetOpt
-import System.Environment (getArgs, withArgs)
+import Options.Applicative
 
--- import Text.Read (readEither)
--- Switch to 'readEither' from "Text.Read" when Debian Stable uses base-4.6.0.0
-import Text.ParserCombinators.ReadP as P
-import Text.ParserCombinators.ReadPrec
-import Text.Read (readPrec)
-
-readEither :: Read a => String -> Either String a
-readEither s =
-  case [ x | (x,"") <- readPrec_to_S read' minPrec s ] of
-    [x] -> Right x
-    []  -> Left "Prelude.read: no parse"
-    _   -> Left "Prelude.read: ambiguous parse"
- where
-  read' =
-    do x <- readPrec
-       lift P.skipSpaces
-       return x
-
--- We request and extra Int parameter 'mdMax' so that
--- it is possible to have per module default benchmark
--- data size.
-commonMain :: Int -> (Int -> [Benchmark]) -> IO ()
+commonMain :: Int                    -- ^ default maximum data size
+           -> (Int -> [Benchmark])   -- ^ the benchmarks to run
+           -> IO ()
 commonMain mdMax bench = do
-    (maybeNewMax, critArgs) <- parseArgs =<< getArgs
-    
-    withArgs critArgs $ defaultMain $ bench $ fromMaybe mdMax maybeNewMax
+    (maybeNewMax, critMode) <- execParser $ info (helper <*> options) mempty
+    runMode critMode $ bench (fromMaybe mdMax maybeNewMax)
 
-options :: [OptDescr (Either String Int)]
+options :: Parser (Maybe Int, Criterion.Mode)
 options =
-    [
-      Option ['i'] ["imax"] (ReqArg readEither "vmax")
-      "Set benchmark maximum data size."
-    ]
-
-parseArgs :: [String] -> IO (Maybe Int, [String])
-parseArgs args =
-    case getOpt Permute options args of
-        ([], rst, [])   -> return (Nothing, rst)
-        ([ev], rst, []) -> either (usrErr prsErrMsg) (validate rst) ev
-        (_, _, errs)    -> usrErr "" $ concat errs
-    where
-        usrErr ce = ioError . userError . (++) ce
-        prsErrMsg = "-i requires a natural number >= 1 : "
-        validate rst v
-            | v >= 1 = return (Just v, rst)
-            | otherwise = usrErr prsErrMsg (show v)
+    (,) <$> optional (option auto (help "benchmark maximum data size"
+                                   <> metavar "N" <> short 'i'  <> long "imax"))
+        <*> Criterion.parseWith Criterion.defaultConfig
