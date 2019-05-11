f2b3deff776e542ec230d8f65b821fa60362568a
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-05-11 09:56:48 -0700

    Build against `semigroups-0.19` (#200)

--- release.nix
+++ release.nix
@@ -19,8 +19,13 @@ let
     };
   };
 
-  pkgs =
-    import <nixpkgs> { inherit config; };
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOs/nixpkgs/archive/312a059bef8b29b4db4e73dc02ff441cab7bb26d.tar.gz";
+
+    sha256 = "1j52yvkhw1inp6ilpqy81xv1bbwgwqjn0v9647whampkqgn6dxhk";
+  };
+
+  pkgs = import nixpkgs { inherit config; };
 
 in
   { pipes = pkgs.haskellPackages.pipes;
