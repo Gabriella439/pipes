4c81ed737973e5c66edb413ea39323ee36b46b76
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-07-07 19:45:09 -0700

    Version 4.3.10 → 4.3.11

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "pipes";
-  version = "4.3.10";
+  version = "4.3.11";
   src = ./.;
   libraryHaskellDepends = [
     base exceptions mmorph mtl semigroups transformers void
